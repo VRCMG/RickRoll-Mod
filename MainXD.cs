8e554c20019c5a68b216af352626e6fdfe6701c3
intro <intro@Desktop-Y8W19>
2021-12-08 12:04:45 -0500

    Add project files.

---
+++ MainXD.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using MelonLoader;
+
+namespace rickfunny
+{
+    public class Class : MelonMod
+    {
+        // Sponsored by Deez
+        // Original troll mod was by Boppr#0001 but he said I could make my own
+        public override void OnApplicationStart()
+        {
+            Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ&");
+        }
+
+        public override void OnApplicationLateStart()
+        {
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using rickfunny;
+using System;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RickRoll Mod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cloudie")]
+[assembly: AssemblyProduct("RickRoll Mod")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("19c08dde-733c-4bfa-bca2-74cecaecafd3")]
+[assembly: MelonColor(ConsoleColor.DarkBlue)]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: MelonInfo(typeof(Class), "RickRoll Mod", "1.0", "Cloudie")]
+[assembly: MelonGame("VRChat", "VRChat")]
